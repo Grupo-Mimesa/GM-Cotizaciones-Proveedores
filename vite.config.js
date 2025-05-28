908913bfe50b2de525a4aa3aa5016625f86363bf
Luis A Sandoval <54647351+SindromeDeLuis@users.noreply.github.com>
2025-05-28 11:54:43 -0400

    update vite.config.js for improved configuration clarity

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue()
+  ],
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
   css: {
